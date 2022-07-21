5837f909ccb2c91608738960dd49470333f2cc23
David Huang <39749287+davidhuangio@users.noreply.github.com>
2022-07-21 14:15:50 -0400

    Update index.ts

--- src/japi/index.ts
+++ src/japi/index.ts
@@ -110,7 +110,7 @@ async function loadEpisode(url: string) {
 function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 async function randomEpisode() {
